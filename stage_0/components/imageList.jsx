5e9adee74b2d3b7cd9e3e5d314b43c0559fd13e9
Hack Reactor Students <accounts@hackreactor.com>
2016-08-28 18:32:18 -0700

    correct file structure and recovered missing files

--- stage_0/components/imageList.jsx
+++ stage_0/components/imageList.jsx
@@ -1,8 +1,11 @@
 var ImageList = (props) => (
-  <div className="image-list">
-    {props.list.map((image, i) =>
-        <ImageListEntry image={image} onClick={props.onClick} key={i} />
-    )}
+  <div>
+    <h1>Puppies!</h1>
+    <div className="image-list">
+      {props.list.map((image, i) =>
+          <ImageListEntry image={image} onClick={props.onClick} key={i} />
+      )}
+    </div>
   </div>
 );
 
--- stage_0/components/imageListEntry.jsx
+++ stage_0/components/imageListEntry.jsx
@@ -1,7 +1,7 @@
 var ImageListEntry = (props) => (
   <div className="image-list-entry">
       <div className="image-list-entry-title" onClick={ function() { props.onClick(props.image.url); } }>
-      {props.image.title}
+      <h3>{props.image.title}</h3>
       </div>
   </div>
 );
